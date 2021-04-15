062a06d1a7a039740cdd7b0cd514593cfa3775dc
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-16 04:08:10 +0800

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,11 +1,11 @@
 let { execSync } = require('child_process')
 let handler = async (m, { conn, text }) => {
- await m.reply('```_Sedang Merestart SGDC-BOT...```')
+ await m.reply('```Sedang Merestart SGDC-BOT...```')
   if (global.conn.user.jid == conn.user.jid) {
  // conn.reply(m.chat, '*_Memulai Ulang BOT..._*', m)
     let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
     require('fs').readdirSync('plugins').map(v=>global.reload('', v))
-    conn.reply(m.chat, '*Info:*\n' + stdout.toString() + '\n\n*SGDC-BOT*', m)
+    conn.reply(m.chat, '*Info Server:*\n' + stdout.toString() + '\n\n*SGDC-BOT*', m)
    }
 }
 
