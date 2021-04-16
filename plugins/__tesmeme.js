3a3f5d7ecef6ac0fd6d7dce7412f5e6e6ed17076
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-16 19:36:57 +0800

    Update __tesmeme.js

--- plugins/__tesmeme.js
+++ plugins/__tesmeme.js
@@ -10,15 +10,15 @@ let api = (kntl.onlydev)
   try {
     await m.reply(global.wait)
     
-    let res = await axios.get(`https://onlydevcity.herokuapp.com/api/mememaker3?teks=${text}&img_url=${anu.display_url}&apikey=${api}`)
-    	    let ress = await fetch(encodeURIComponent(res.data.result));
+          let res = await axios.get(`https://onlydevcity.herokuapp.com/api/mememaker3?teks=${text}&img_url=${anu.display_url}&apikey=${api}`)
+    	    let ress = await fetch(res.data.result);
             //let buf = Buffer.from(ress, 'base64')
-            let str = `*SGDC-BOT*`
+          let str = `*SGDC-BOT*`
           conn.sendFile(m.chat, ress, 'Nyenye_SGDC-BOT.jpg', str, m)
     
-    /*let res = await axios.get(`https://onlydevcity.herokuapp.com/api/mememaker3?teks=${text}&img_url=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaVPHWMTO7jGoZP0QHiqlbODT9Gjxo1HnSug&usqp=CAU&apikey=${api}`)
+    //let res = await axios.get(`https://onlydevcity.herokuapp.com/api/mememaker3?teks=${text}&img_url=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaVPHWMTO7jGoZP0QHiqlbODT9Gjxo1HnSug&usqp=CAU&apikey=${api}`)
           
-          conn.sendMessage(m.chat, res.data.result, MessageType.sticker, {
+        / conn.sendMessage(m.chat, ress, MessageType.sticker, {
     quoted: m
   })     
   //  })
