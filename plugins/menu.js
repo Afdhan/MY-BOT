b39bff464366757dd2b94d3ba54711151c59ab94
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-17 02:20:23 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -403,7 +403,7 @@ ${readMore}
   let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
   await  conn.reply(m.chat, mn, m, { 
         contextInfo: { 
-            mentionedJid: users, [mmk]
+            mentionedJid: users, mmk
          }
      })
    }
