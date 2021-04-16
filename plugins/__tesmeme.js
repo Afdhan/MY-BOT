3c07f1df411a35f401aabaf9efda981072a9a8ab
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-16 19:08:59 +0800

    Update __tesmeme.js

--- plugins/__tesmeme.js
+++ plugins/__tesmeme.js
@@ -1,5 +1,6 @@
 let imageToBase64 = require('image-to-base64');
 let axios = require("axios");
+let { MessageType } = require('@adiwajshing/baileys');
 let kntl = require("../src/kntl.json");
 let handler = async(m, { conn, text }) => {
 let api = (kntl.onlydev)
@@ -7,20 +8,11 @@ let api = (kntl.onlydev)
     await m.reply(global.wait)
     axios.get(`https://onlydevcity.herokuapp.com/api/mememaker3?teks=${text}&img_url=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaVPHWMTO7jGoZP0QHiqlbODT9Gjxo1HnSug&usqp=CAU&apikey=${api}`)
     .then((res) => {
-    /*  imageToBase64(res.data.result)
-        .then(
-          (ress) => {
-            let buf = Buffer.from(ress, 'base64')*/
-            let str = `*SGDC-BOT*`
-          conn.sendFile(m.chat, encodeURIComponent(res.data.result)), 'Nyenye_SGDC-BOT.png', str, m)
-       // })
-    })
-      /* let triger = await fetch('https://some-random-api.ml/canvas/triggered?avatar=' + encodeURIComponent(anu.display_url));
-       let img = await triger.buffer()
-       let stiker = await sticker(img, false, global.packname, global.author)
-       conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+          let str = `*SGDC-BOT*`
+          conn.sendMessage(m.chat, encodeURIComponent(res.data.result)), MessageType.sticker, {
     quoted: m
-  })*/
+  })     
+    })
     } catch (e) {
   	m.reply('```Error```')
   }
