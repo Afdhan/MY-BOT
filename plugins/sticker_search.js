d39e7523dd4f82c9e98876823224f7397e79c168
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-16 14:35:02 +0700

    Add files via upload

---
+++ plugins/sticker_search.js
@@ -0,0 +1,39 @@
+let axios = require("axios");
+let { MessageType } = require('@adiwajshing/baileys');
+let { sticker } = require('../lib/sticker');
+let kntl = require("../src/kntl.json")
+let handler = async(m, { conn, text }) => {
+	let api = (kntl.xteam)
+	if (!text) throw '_Masukkan Teks_'
+try {
+    await m.reply(global.wait)
+	axios.get(`https://api.xteam.xyz/sticker/stickerly?q=${text}&APIKEY=${api}`)
+    .then ((res) => {
+    	let stic = res.data.result.stickerlist
+		let b = JSON.parse(JSON.stringify(stic.data));
+		let stick =  b[Math.floor(Math.random() * b.length)];
+		let stiker = await sticker(stick, false, global.packname, global.author)
+       conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+        })
+     }
+   } catch (e) {
+  	m.reply('```Error```')
+  }
+}
+handler.command = /^(stickersearch|ssearch)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+
+module.exports = handler
+
+//  MUHAMMAD AFDHAN
