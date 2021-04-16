5734b79dc74611574430ec53fb896f7cb67b9fb6
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-16 20:36:06 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,13 +1,13 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, args }) => {
-  let users1 = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
-  let users2 = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
   if (!m.isGroup) {
     await conn.sendContact(m.chat, '6282252655313', 'Owner SGDC-BOT', m)
-    conn.sendContact(m.chat, '6283129011845', 'Co-Owner SGDC-BOT', m)
-  } else {
+    conn.sendContact(m.chat, '6283129011845', 'Co_Owner SGDC-BOT', m)
+} else {
+  let users1 = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+  let users2 = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
   await conn.sendContact(m.chat, '6282252655313', 'Owner SGDC-BOT', m, { contextInfo: { mentionedJid: users1 } })
-  conn.sendContact(m.chat, '6283129011845', 'Co-Owner SGDC-BOT', m, { contextInfo: { mentionedJid: users2 } })
+  conn.sendContact(m.chat, '6283129011845', 'Co_Owner SGDC-BOT', m, { contextInfo: { mentionedJid: users2 } })
   }
 }
 
