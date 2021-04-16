e423c328492a754cc7fdcc4a8cb746e5dad592a4
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-16 20:29:45 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,8 +1,13 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, args }) => {
-  let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
-  conn.sendContact(m.chat, '6282252655313', 'Owner SGDC-BOT', m, { contextInfo: { mentionedJid: users } })
-m.reply('wa.me/6282252655313')
+  let users1 = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+  let users2 = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+  if (!m.isGroup) {
+    await conn.sendContact(m.chat, '6282252655313', 'Owner SGDC-BOT', m)
+    conn.sendContact(m.chat, '6283129011845', 'Co-Owner SGDC-BOT', m)
+  } else {
+  await conn.sendContact(m.chat, '6282252655313', 'Owner SGDC-BOT', m, { contextInfo: { mentionedJid: users1 } })
+  conn.sendContact(m.chat, '6283129011845', 'Co-Owner SGDC-BOT', m, { contextInfo: { mentionedJid: users2 } })
 }
 
 handler.command = /^(owner|creator)$/i
