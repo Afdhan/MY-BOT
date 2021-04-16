77f0ac10cafde09849664882c907740ac8e9de21
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-16 15:45:14 +0800

    Update sticker_search.js

--- plugins/sticker_search.js
+++ plugins/sticker_search.js
@@ -7,20 +7,19 @@ let handler = async(m, { conn, args, text }) => {
 	if (!text) throw '_Masukkan Teks_'
 //try {
     await m.reply(global.wait)
-	axios.get(`https://api.xteam.xyz/sticker/stickerly?q=${text}&APIKEY=${api}`)
-    .then ((res) => {
+        let res = await axios.get(`https://api.xteam.xyz/sticker/stickerly?q=${text}&APIKEY=${api}`)
     	let stic = res.data.result.stickerlist
-		let b = JSON.parse(JSON.stringify(stic.data));
-		let img =  b[Math.floor(Math.random() * b.length)];
+	let b = JSON.parse(JSON.stringify(stic.data));
+	let img =  b[Math.floor(Math.random() * b.length)];
 //let stiker = await sticker(img)  //, false, global.packname, global.author)
        conn.sendMessage(m.chat, img, MessageType.sticker, {
     quoted: m
         })
      }
    /*} catch (e) {
-  	m.reply('```Error```')
-  }*/
-}
+  	m.reply('```Error```')*/
+ /* }
+}*/
 handler.command = /^(stickersearch|ssearch)$/i
 handler.owner = false
 handler.mods = false
