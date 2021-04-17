ec7822f1afcc59f834f09c0a0e29ed16b92e4f36
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-17 23:31:31 +0700

    Update rp_mark.js

--- plugins/rp_mark.js
+++ plugins/rp_mark.js
@@ -3,7 +3,8 @@ const { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, '_Masukkan Text Laporan!_', m)
     if (text.length > 200) return conn.reply(m.chat, '_Teks Terlalu Panjang!, Maksimal 200 Karakter_', m)
-    conn.sendMessage('0@s.whatsapp.net', `${text}`, MessageType.text)
+    await conn.sendMessage('0@s.whatsapp.net', `${text}`, MessageType.text)
+    m.reply('_Berhasil Mengirim Pesan Ke Mark!_')
 }               
 
 
