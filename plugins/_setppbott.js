39b9542496784d4f75e9d73d4d11635d23a0782b
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-17 21:52:12 +0700

    Update _setppbott.js

--- plugins/_setppbott.js
+++ plugins/_setppbott.js
@@ -1,5 +1,5 @@
 let { Presence } = require('@adiwajshing/baileys')
-let handler = async(m, { conn, text, args, bot, command, usedPrefix }) => {
+let handler = async(m, { conn, text, args, bot, isOwner, command, usedPrefix }) => {
     await m.reply(global.wait)
     const type = Object.keys(m.message)[0]
     const content = JSON.stringify(m.message)
@@ -7,14 +7,17 @@ let handler = async(m, { conn, text, args, bot, command, usedPrefix }) => {
     const isQuotedImage = type === 'extendedTextMessage' && content.includes('imageMessage')
     const isQuotedVideo = type === 'extendedTextMessage' && content.includes('videoMessage')
          try {
-         	       if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
-					if (!isOwner) return m.reply('```Anda Siapa?```')
-					conn.updatePresence(m.chat, Presence.composing) 
-					if (!isQuotedImage) return m.reply(`_Kirim gambar dengan caption ${usedPrefix}setppbot atau tag gambar yang sudah dikirim_`)
-					enmedia = JSON.parse(JSON.stringify(m).replace('quotedM','m')).message.extendedTextMessage.contextInfo
-					media = await conn.downloadAndSaveMediaMessage(enmedia)
-					await conn.updateProfilePicture(bot, media)
-					m.reply('_Mwhehehe_')
+         if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
+         if (!isOwner) return m.reply('```Anda Siapa?```')
+         conn.updatePresence(m.chat, Presence.composing) 
+         if (!isQuotedImage) return m.reply(`_Kirim gambar dengan caption ${usedPrefix}setppbot atau tag gambar yang sudah dikirim_`)
+         enmedia = JSON.parse(JSON.stringify(m).replace('quotedM','m')).message.extendedTextMessage.contextInfo : m
+         media = await conn.downloadAndSaveMediaMessage(enmedia)
+         await conn.updateProfilePicture(bot, media)
+         m.reply('_Mwhehehe_')
+  } else {
+        m.reply('_Reply Fotonya!_')
+        }
 	 } catch (e) {
    m.reply('```Error```')
    console.log(e)
