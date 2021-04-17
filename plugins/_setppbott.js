c981a7374093681f18d37e92a2fefff319d5a1ab
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-17 21:48:04 +0700

    Add files via upload

---
+++ plugins/_setppbott.js
@@ -0,0 +1,34 @@
+let { Presence } = require('@adiwajshing/baileys')
+let handler = async(m, { conn, text, args, bot, command, usedPrefix }) => {
+    await m.reply(global.wait)
+    const type = Object.keys(m.message)[0]
+    const content = JSON.stringify(m.message)
+    const isMedia = (type === 'imageMessage' || type === 'videoMessage')
+    const isQuotedImage = type === 'extendedTextMessage' && content.includes('imageMessage')
+    const isQuotedVideo = type === 'extendedTextMessage' && content.includes('videoMessage')
+         try {
+         	       if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
+					if (!isOwner) return m.reply('```Anda Siapa?```')
+					conn.updatePresence(m.chat, Presence.composing) 
+					if (!isQuotedImage) return m.reply(`_Kirim gambar dengan caption ${usedPrefix}setppbot atau tag gambar yang sudah dikirim_`)
+					enmedia = JSON.parse(JSON.stringify(m).replace('quotedM','m')).message.extendedTextMessage.contextInfo
+					media = await conn.downloadAndSaveMediaMessage(enmedia)
+					await conn.updateProfilePicture(bot, media)
+					m.reply('_Mwhehehe_')
+	 } catch (e) {
+   m.reply('```Error```')
+   console.log(e)
+ }
+}
+					
+handler.command = /^(group|grup|gc)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+handler.admin = true
+handler.botAdmin = true
+handler.fail = null
+
+module.exports = handler
