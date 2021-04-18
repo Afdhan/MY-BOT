ff451ea4b0e9e3acf75546f6eba6b4770a280f45
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-19 03:30:40 +0800

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -5,8 +5,11 @@ let chat = global.DATABASE.data.chats[m.chat]
 if (chat.simi) {
         axios.get(`https://videfikri.com/api/simsimi/?teks=${text}`).then ((res) => {
         let simih = res.data.result
-	if (simih.status == '200') conn.reply(m.chat, simih.jawaban, m)
-        else throw 'Ngomong Apa?'
+	if (simih.status == '200') {
+	conn.reply(m.chat, simih.jawaban, m)
+        } else {
+	m.reply('Ngomong Apa?') 
+	}
       })
   } else throw '*SIMI BELUM DIAKTIFKAN UNTUK CHAT INI!*'
 }
