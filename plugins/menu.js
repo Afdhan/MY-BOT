1671d64ccd5da5b0094b2e2d8cede0ce21c67210
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-18 22:01:29 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -396,6 +396,8 @@ ${readMore}
   ${desc} *${bname}@^${vers}*
 
 `.trim()
+     let wew = fs.readFileSync('./src/SGDC_BOT.jpg')
+     conn.sendMessage(m.chat, wew, MessageType.image, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "*SUPPORT ME WITH DONATE*", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('src/SGDC_BOT.jpg')} } }, caption: mn })
   /*  await conn.sendFile(m.chat, poto, 'SGDC-BOT.jpg', mn, m, false, { 
         contextInfo: { 
             mentionedJid: [kntl, own]
