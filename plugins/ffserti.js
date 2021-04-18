50e20b9887a1c631b4af29a7727020e8ab30c468
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-18 15:40:59 +0800

    Update ffserti.js

--- plugins/ffserti.js
+++ plugins/ffserti.js
@@ -1,11 +1,21 @@
+let { sticker } = require('../lib/sticker');
+let { MessageType } = require('@adiwajshing/baileys');
+let { spawn } = require('child_process');
 let axios = require("axios");
+let path = require('path');
+let util = require('util');
 let handler = async(m, { conn, text }) => {
  try {
     if (!text) return conn.reply(m.chat, '_Masukan Nama!_', m)
     if (text.length > 15) return conn.reply(m.chat, '_Teks Terlalu Panjang! Maksimal 15 huruf!_', m)
    await m.reply(global.wait)
    let link = 'https://onlydevcity.xyz/FFSerti/img.php?nama=' + text;
-   conn.sendFile(m.chat, link, 'SGDC-BOT.jpeg', '*SGDC-BOT*', m)
+       conn.sendFile(m.chat, link, 'SGDC-BOT.jpeg', '*SGDC-BOT*', m)
+   let img = await link.buffer()
+   let stiker = await sticker(img, false, global.packname, global.author)
+       conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
    } catch (e) {
    m.reply('```Error```')
   }
