66b0fe3278b84a2f1925477d13fb5ba3a6931a79
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-19 13:26:37 +0800

    Update search_ps.js

--- plugins/search_ps.js
+++ plugins/search_ps.js
@@ -3,17 +3,20 @@ let kntl = require("../src/kntl.json")
 let handler = async(m, { conn, text }) => {
       if (!text) return conn.reply(m.chat, '_Mau Cari Apa Tod?_', m)
       let api = (kntl.onlydev)
-	  await m.reply(global.wait)
+      await m.reply(global.wait)
   try {
   	axios.get(`https://onlydevcity.herokuapp.com/api/googleplay?q=${text}&apikey=${api}`).then ((res) => {
   	let tytyd = res.data.result.data
-      let ppk = `*「  PLAYSTORE SEARCH  」*\n\n`
-      for (let i = 0; i < tytyd.result.length; i++) {
+        let ppk = `*「  PLAYSTORE SEARCH  」*\n\n`
+        for (let i = 0; i < tytyd.result.length; i++) {
       	ppk += `─────────────────\n• *Title:* ${tytyd.result[i].title}\n• *AppId:* ${tytyd.result[i].appId}\n• *Developer:* ${tytyd.result[i].developer}\n• *Link:* ${tytyd.result[i].url}\n`
                 }
-          ppk += '\n\n*SGDC-BOT*'
+        ppk += '\n\n*SGDC-BOT*'
                  conn.reply(m.chat, sfile, m)
 	})
+    } catch (e) {
+  	m.reply('```Error```')
+  }
 }
 handler.command = /^(playstore|ps)$/i
 handler.owner = false
