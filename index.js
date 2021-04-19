3c99835cc0aeb19ac7107ffa5cb5f849fda1b924
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-20 01:54:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ CFonts.say(`${package.name}`, {
   align: 'center',
   colors: ['red'],
   background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
-	letterSpacing: 0,           // define letter spacing
+	letterSpacing: 1,           // define letter spacing
 	lineHeight: 1,              // define the line height
 	space: false,                // define if the output text should have empty lines on top and on the bottom
-	maxLength: '8',             // define how many character can be on one line
+	maxLength: '0',             // define how many character can be on one line
 	gradient: false,            // define your two gradient colors
 	independentGradient: false, // define if you want to recalculate the gradient for each new line
 	transitionGradient: false,  // define if this is a transition between colors directly
