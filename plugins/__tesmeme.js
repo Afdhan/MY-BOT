ed5a8a933083a1d42671e6a4e63669a05cda808c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-19 12:34:50 +0800

    Add files via upload

--- plugins/__tesmeme.js
+++ plugins/__tesmeme.js
@@ -1,31 +1,37 @@
+let imgBB = require("imgbb-uploader");
 let imageToBase64 = require('image-to-base64');
-let axios = require("axios");
-let path = require("path")
-let util = require("util")
-let fetch = require('node-fetch');
+let { sticker } = require('../lib/sticker');
 let { MessageType } = require('@adiwajshing/baileys');
-let kntl = require("../src/kntl.json");
-let handler = async(m, { conn, args, text }) => {
-let api = (kntl.onlydev)
-  try {
+let { spawn } = require('child_process');
+let FormData = require('form-data');
+let fetch = require('node-fetch');
+let path = require('path');
+let util = require('util');
+let fs = require('fs');
+let axios = require("axios");
+let handler = async(m, { conn, text, args, bot, command }) => {
     await m.reply(global.wait)
-          let api = (kntl.onlydev)
-          let url = 'https://i.ibb.co/1T1DCz7/cewek-thailand-20200325-007-non-fotografer-kly.jpg'
-          let res = await axios.get(`https://onlydevcity.herokuapp.com/api/mememaker3?teks=${text}&img_url=${url}&apikey=${api}`)
-    	    let ress = res.data.result
-            //let buf = Buffer.from(ress, 'base64')
-          let str = `*SGDC-BOT*`
-          conn.sendFile(m.chat, ress, 'Nyenye_SGDC-BOT.jpg', str, m)
-    
-    //let res = await axios.get(`https://onlydevcity.herokuapp.com/api/mememaker3?teks=${text}&img_url=https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaVPHWMTO7jGoZP0QHiqlbODT9Gjxo1HnSug&usqp=CAU&apikey=${api}`)
-          
-        / conn.sendMessage(m.chat, ress, MessageType.sticker, {
-    quoted: m
-  })     
-  //  })
+    const type = Object.keys(m.message)[0]
+    const content = JSON.stringify(m.message)
+    const isMedia = (type === 'imageMessage' || type === 'videoMessage')
+    const isQuotedImage = type === 'extendedTextMessage' && content.includes('imageMessage')
+    const isQuotedVideo = type === 'extendedTextMessage' && content.includes('videoMessage')
+ try {
+    var imgBB = require('imgbb-uploader')
+    if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
+        ngntd = isQuotedImage ? JSON.parse(JSON.stringify(m).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : m
+        media = await conn.downloadAndSaveMediaMessage(ngntd)
+        anu = await imgBB("3ea1465ef91578a90ee81f7d41c59a1f", media)
+
+            let res = await fetch(`https://onlydevcity.herokuapp.com/api/mememaker3?teks=${text}&img_url=${anu.display_url}&apikey=${api}`)
+    	    let ress = await imageToBase64(res.data.result.url)
+            let buf = Buffer.from(ress, 'base64')
+            let str = `*SGDC-BOT*`
+          conn.sendFile(m.chat, buf, 'Nyenye_SGDC-BOT.jpg', str, m)
+       
+    })
     } catch (e) {
   	m.reply('```Error```')
-      console.log(e)
   }
 }
 
