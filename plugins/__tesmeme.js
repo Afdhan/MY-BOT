82aec625b01027b670ec58e068afa5cd4d44b6aa
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-19 12:38:07 +0800

    Update __tesmeme.js

--- plugins/__tesmeme.js
+++ plugins/__tesmeme.js
@@ -10,6 +10,7 @@ let util = require('util');
 let fs = require('fs');
 let axios = require("axios");
 let handler = async(m, { conn, text, args, bot, command }) => {
+    if(!text) m.reply('_Masukkan Teks!_')
     await m.reply(global.wait)
     const type = Object.keys(m.message)[0]
     const content = JSON.stringify(m.message)
@@ -24,7 +25,7 @@ let handler = async(m, { conn, text, args, bot, command }) => {
         anu = await imgBB("3ea1465ef91578a90ee81f7d41c59a1f", media)
 
             let res = await fetch(`https://onlydevcity.herokuapp.com/api/mememaker3?teks=${text}&img_url=${anu.display_url}&apikey=${api}`)
-    	    let ress = await imageToBase64(res.data.result.url)
+    	    let ress = await imageToBase64(res.data.result)
             let buf = Buffer.from(ress, 'base64')
             let str = `*SGDC-BOT*`
           conn.sendFile(m.chat, buf, 'Nyenye_SGDC-BOT.jpg', str, m)
