48464ef40cd25302a1fbe77dd3d65a323a6f3d12
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-19 23:30:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('MUHAMMAD AFDHAN', {
+/*CFonts.say('Ada Satu Yang Harus Kamu Perhatikan', {
   font: 'console',
   align: 'center',
   colors: ['red'],
   gradient: false
-})
+})*/
 CFonts.say(`${package.name}`, {
   font: 'block',
   align: 'center',
@@ -23,11 +23,11 @@ CFonts.say(`Karena suatu Kejadian berawal dari sebuah Tindakan.|Jadi, jangan rag
 })
 function start(file) {
   let args = [path.join(__dirname, file), ...process.argv.slice(2)]
-  CFonts.say([process.argv[0], ...args].join(' '), {
+  /*CFonts.say([process.argv[0], ...args].join(' '), {
     font: 'console',
     align: 'center',
     gradient: ['blue', 'red']
-  })
+  })*/
   let p = spawn(process.argv[0], args, {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc']
   })
