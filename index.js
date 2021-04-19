fabe748c88a705e8eeca3905b359cf40eef3395e
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-20 02:12:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const CFonts  = require('cfonts')
 CFonts.say(`${package.name}`, {
   font: 'pallet',
   align: 'center',
-  colors: ['red','cyan'],
+  colors: ['red','green'],
   background: 'transparent',        // define the background color, you can also use `backgroundColor` here as key
-	letterSpacing: 0,           // define letter spacing
-	lineHeight: 0,              // define the line height
+	letterSpacing: 1,           // define letter spacing
+	lineHeight: 1,              // define the line height
 	space: false,               // define if the output text should have empty lines on top and on the bottom
 	maxLength: '0',             // define how many character can be on one line
 	gradient: false,            // define your two gradient colors
