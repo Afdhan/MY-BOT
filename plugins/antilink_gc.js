b309bbef16b4154dededf6b407a4dfad61fafd16
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-21 22:37:24 +0700

    Update antilink_gc.js

--- plugins/antilink_gc.js
+++ plugins/antilink_gc.js
@@ -1,9 +1,9 @@
 let handler = async function(m, { conn , args, isAdmin, isBotAdmin, usedPrefix }) {
   let ajg = m.sender
   let chat = global.DATABASE.data.chats[m.chat]
- if(!m.isGroup) return false
+// if(!m.isGroup) return false
  if (chat.antiLink && m.isGroup) {
- if (m.isBaileys && m.fromMe) throw false
+ //if (m.isBaileys && m.fromMe) throw false
  if (isAdmin) return m.reply('_Kali ini anda aman! Karena anda adalah Admin !!!_')
  await conn.reply(m.chat, `
 *[ CHAT ANTI LINK ]*
@@ -19,14 +19,13 @@ Maaf Kamu akan dikick oleh *SGDC-BOT*
  if(isBotAdmin) { 
    conn.groupRemove(m.chat, [ajg])
     } else if (!isBotAdmin) return m.reply('_Jadikan SGDC-BOT Sebagai Admin, Agar Bisa Bertindak !!!_')
-    } else {
-      m.reply('```AKTIFIN ANTILINK! BIAR GUA ULTI NI BOCAH !!!```')
-    }
+   } //else m.reply('```AKTIFIN ANTILINK! BIAR GUA ULTI NI BOCAH !!!```')
+    
  }
 
 handler.customPrefix = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 handler.command = new RegExp
-
+handler.group = true
 handler.fail = null
 
 module.exports = handler
