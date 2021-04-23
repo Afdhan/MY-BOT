31564dd2ee333e222103c0d1913a3a7e99afe2ab
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-24 02:07:29 +0700

    Update _%TESF.js

--- plugins/_%TESF.js
+++ plugins/_%TESF.js
@@ -3,8 +3,11 @@ let handler = async(m, { conn, command, text }) => {
       if(command == 'search' || command == 'srch') {
        let resp = await conn.searchMessages(`${text}`, m.chat, 25, 1)
        await conn.reply(m.chat, `Hasil Pencarian Pesan ${text},\nAda *${resp.messages.length}* Pesan Ditemukan`, m)
-       let tt = Object.values(resp.messages.length).map(v => '- ' + v).join('\n')
-       m.reply(tt)
+       /*let tt = Object.values(resp.messages.length).map(v => '- ' + v).join('\n')
+       m.reply(tt)*/
+             let _m = await conn.serializeM(resp.messages.length)
+                                       
+              _m.copyNForward(m.chat, true)
          //conn.copyNForward(m.chat, m.message)
      } else if(command == 'load' || command == 'loadmsg') {
         let user = m.mentionedJid[0]
