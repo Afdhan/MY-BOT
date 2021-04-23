3031abf1eebe558c87b6d6719726d652c3d3dee1
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-24 03:59:55 +0700

    Update kontag.js

--- plugins/kontag.js
+++ plugins/kontag.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
 		  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted : m.fromMe ? conn.user.jid : m.sender 
 		  let user = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
 		  let name = conn.getName(who)
-		 conn.sendContact(m.chat, who.split("@s.whatsapp.net")[0], `${name}, m,{ contextInfo: { mentionedJid: user } })
+		 conn.sendContact(m.chat, who.split("@s.whatsapp.net")[0], `${name}, m, { contextInfo: { mentionedJid: user } })
 		} catch(e) {
     m.reply('Error')
    }
