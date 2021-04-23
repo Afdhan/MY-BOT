9e5ee0c3725ca79d0f778a63bd4114f1129db027
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-24 01:29:21 +0700

    Update _%TESF.js

--- plugins/_%TESF.js
+++ plugins/_%TESF.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, text }) => {
    try{
        let resp = await conn.searchMessages(`${text}`, m.chat, 25, 1)
-       conn.reply(m.chat, 'Hasil' + resp.messages.length, m)
+       conn.reply(m.chat, `Hasil Pencarian Pesan ${text},\nAda *${resp.messages.length}* Pesan Ditemukan`, m)
        } catch (e) {
     m.reply('```Error```')
     console.log (e)
