e4e324bec1da3b2b95261d9900189a2c16595db0
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-24 21:30:14 +0700

    TT

--- plugins/__BuatGC.js
+++ plugins/__BuatGC.js
@@ -3,9 +3,9 @@ let handler = async(m, { conn, text }) => {
    await m.reply(global.wait)
    try{
     let group = await conn.groupCreate(text, [m.sender || global.mods])
-    console.log ("Membuat Grup: " + group.gid)
+    console.log ('Membuat Grup: ' + group.gid + 'Nama Grup: ' + text)
     //conn.sendMessage(group.gid, "Success to group create!", MessageType.extendedText)
-     m.reply('_Berhasil Membuat Grup_ *' + group.gid + '*')
+     m.reply('_Berhasil Membuat Grup *' + text + '*_\nID: *' + group.gid + '*')
        } catch (e) {
     m.reply('```Error```')
     console.log (e)
