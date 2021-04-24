88d24113cc9e69aeaa634b5620b514c1b64d5b79
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-24 20:13:35 +0700

    Update __BuatGC.js

--- plugins/__BuatGC.js
+++ plugins/__BuatGC.js
@@ -1,10 +1,13 @@
 let MessageType = require('@adiwajshing/baileys');
 let handler = async(m, { conn, text }) => {
    try{
-    let group = await conn.groupCreate(text, [m.sender || global.mods])
-    console.log ("created group with id: " + group.gid)
+    let nmr = m.sender
+    let mor = nmr.split("@s.whatsapp.net")[0]
+    let ttk = 'mor@s.whatsapp.net'
+    let group = await conn.groupCreate(text, [ttk])
+    console.log ('Membuat Grup' + text)
     conn.sendMessage(group.gid, "Success to group create!", MessageType.extendedText)
-     m.reply('_Berhasil Membuat Grup_ *' + group.subject + '*')
+     m.reply('_Berhasil Membuat Grup_ *' + group.gid + '*')
        } catch (e) {
     m.reply('```Error```')
     console.log (e)
