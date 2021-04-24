7cdcf29926b2fe657e03bbae2d642ad6ce07dbd5
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-24 19:57:52 +0700

    Add files via upload

---
+++ plugins/__BuatGC.js
@@ -0,0 +1,16 @@
+let MessageType = require('@adiwajshing/Baileys');
+let handler = async(m, { conn, text }) => {
+   try{
+    let group = await conn.groupCreate(text, [m.sender || global.mods])
+    console.log ("created group with id: " + group.gid)
+    conn.sendMessage(group.gid, "Success to group create!", MessageType.extendedText)
+     m.reply('_Berhasil Membuat Grup_ *' + group.subject + '*')
+       } catch (e) {
+    m.reply('```Error```')
+    console.log (e)
+  }
+}
+
+handler.command = /^((create|buat)(gc|grup|group))$/
+
+module.exports = handler
