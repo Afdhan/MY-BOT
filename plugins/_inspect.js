33d0bac0a27f2414c99dd65317533a42804c9809
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-04-25 10:19:21 +0700

    Update _inspect.js

--- plugins/_inspect.js
+++ plugins/_inspect.js
@@ -8,13 +8,13 @@ let handler = async (m, { conn, text }) => {
     expect200: true
   })
   let caption = `
-*[ • GROUP LINK INSPECTOR • ]*
+*[   GROUP LINK INSPECTOR   ]*
 
 *> Nama Grup:* ${res.subject}
 *> ID:* ${res.id}
 *> Dibuat Pada:* ${formatDate(res.creation * 1000)}
-*> Nama Diubah Oleh:* @${res.subjectOwner.split`@`[0]} *Pada Tanggal* ${formatDate(res.subjectTime * 1000)}
-*> Deskripsi Dibuat Oleh:* @${res.descOwner.split`@`[0]} *pada Tanggal* ${formatDate(res.descTime * 1000)}
+*> Nama Diubah Oleh:* @${res.subjectOwner.split`@`[0]} *Pada* ${formatDate(res.subjectTime * 1000)}
+*> Deskripsi Dibuat Oleh:* @${res.descOwner.split`@`[0]} *Pada* ${formatDate(res.descTime * 1000)}
 *> Deskripsi:*
 ${res.desc}
 
