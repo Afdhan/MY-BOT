a75f97a629ef378161ab22e80033baff5ad547aa
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-04 20:52:45 +0700

    Update __BuatGC.js

--- plugins/__BuatGC.js
+++ plugins/__BuatGC.js
@@ -8,9 +8,9 @@ let handler = async(m, { conn, text }) => {
     let group = await conn.groupCreate(text, [m.sender])
     let link = await conn.groupInviteCode(group.gid)
     let url = 'https://chat.whatsapp.com/' + link;
-    console.log(chalk.bold.green('Membuat Grup: ' + group.gid + '\nNama Grup: ' + text + '\n\nSGDC-BOT'))
+    console.log(chalk.bold.red('Membuat Grup: ' + group.gid + '\nNama Grup: ' + text + '\n\nSGDC-BOT'))
     //conn.sendMessage(group.gid, "Success to group create!", MessageType.extendedText)
-     m.reply('_Berhasil Membuat Grup *' + text + '*_\n\n*Nama:* ' + text + '\n*ID:* ' + group.gid + '\n*Link:* ' + url)
+     m.reply('_Berhasil Membuat Grup *' + text + '*_\n\n*Nama:* ' + text + '\n*ID:* ' + group.gid + '\n*Link:* ' + url + '\n\n*SGDC-BOT*')
        } catch (e) {
     m.reply('```Error```')
     console.log (e)
