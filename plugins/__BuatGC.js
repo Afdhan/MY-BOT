df7d76beb1d44f969fb002fa92ead3c0c3ee91e2
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-04 20:48:59 +0700

    Update __BuatGC.js

--- plugins/__BuatGC.js
+++ plugins/__BuatGC.js
@@ -1,16 +1,14 @@
 let MessageType = require('@adiwajshing/baileys');
+let chalk = require("chalk");
 let handler = async(m, { conn, text }) => {
    
    if (!text) return m.reply('_Masukkan Nama Grup!_')
    try{
          await m.reply(global.wait)
     let group = await conn.groupCreate(text, [m.sender])
-    //let user = m.sender.split("@s.whatsapp.net")[0]
-    //let users = user.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v.length > 20)
-         //await conn.groupAdd(group.gid, users)
     let link = await conn.groupInviteCode(group.gid)
     let url = 'https://chat.whatsapp.com/' + link;
-    console.log ('Membuat Grup: ' + group.gid + '\nNama Grup: ' + text)
+    console.log(chalk.bold.green('Membuat Grup: ' + group.gid + '\nNama Grup: ' + text + '\n\nSGDC-BOT'))
     //conn.sendMessage(group.gid, "Success to group create!", MessageType.extendedText)
      m.reply('_Berhasil Membuat Grup *' + text + '*_\n\n*Nama:* ' + text + '\n*ID:* ' + group.gid + '\n*Link:* ' + url)
        } catch (e) {
