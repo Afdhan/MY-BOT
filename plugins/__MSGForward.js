9ed1a04f017e79216300825597bdfa717449f695
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-05 20:16:59 +0700

    Update __MSGForward.js

--- plugins/__MSGForward.js
+++ plugins/__MSGForward.js
@@ -1,11 +1,11 @@
 
 let handler = async(m, { conn, text }) => {
 let txt = m.quoted ? m.quoted.text ? m.quoted.text : text ? text : m.text : text ? text : m.text
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted : m.fromMe ? conn.user.jid : m.sender
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted : m.fromMe ? conn.user.jid : m.quoted
 let memek = who.split("@s.whatsapp.net")[0]
 
-const messages = await conn.loadConversation ('1234@s.whatsapp.net', 1)
-const message = messages[0] // get the last message from this conversation
+//const messages = await conn.loadConversation ('1234@s.whatsapp.net', 1)
+//const message = messages[0] // get the last message from this conversation
 
 await conn.forwardMessage(memek + '@s.whatsapp.net', txt) // WA forward the message!
 
