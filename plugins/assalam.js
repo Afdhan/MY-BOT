26e547149d992a7888708d00f45ba6ad5a06a01d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-07 02:22:26 +0700

    Update assalam.js

--- plugins/assalam.js
+++ plugins/assalam.js
@@ -1,12 +1,12 @@
 let util = require('util')
 let path = require('path')
-let toPTT = require("../lib/converter");
+let convert = require("../lib/converter");
 let { spawn } = require('child_process')
 
 let handler = async (m, { conn, args }) => {
 //let user = m.sender
   let vnn ='src/SALAM.opus'
-  let toPTT = require("../lib/converter");
+  let toPTT = convert.toPTT
   let vn = await toPTT(vnn, true)
   //m.reply('_Waalaikumsalam Kak @${user.split("@")[0]} :)_')
   conn.sendFile(m.chat, vn, 'Assaalamualaikum.opus', null, m, true)
