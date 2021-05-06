01dd47f6de8890485220b5c303b6832c96009962
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-07 02:37:49 +0700

    Update assalam.js

--- plugins/assalam.js
+++ plugins/assalam.js
@@ -1,15 +1,16 @@
 let util = require('util')
 let path = require('path')
+let fs = require("fs")
 let convert = require("../lib/converter");
 let { spawn } = require('child_process')
 
 let handler = async (m, { conn, args }) => {
 //let user = m.sender
-  let vnn ='src/SALAM.m4a'
+  /*let vnn ='src/SALAM.m4a'
   let toPTT = convert.toPTT
-  let vn = await toPTT(vnn, vnn)
+  let vn = await toPTT(vnn, vnn)*/
   //m.reply('_Waalaikumsalam Kak @${user.split("@")[0]} :)_')
-  conn.sendFile(m.chat, vn, 'Assaalamualaikum.opus', null, m, true)
+  conn.sendFile(m.chat, { url: fs.readFileSync('./src/SALAM.upus') }, 'Assaalamualaikum.opus', null, m, true)
 }
 handler.command = new RegExp
 handler.customPrefix = /^(p(unten|ermisi)?|samlekom|hai|halo|hallo)$/i
