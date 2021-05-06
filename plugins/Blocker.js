9d94e166af13a5e0f2eaefb096ccafeedde35dab
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-06 16:47:02 +0700

    Update Blocker.js

--- plugins/Blocker.js
+++ plugins/Blocker.js
@@ -2,14 +2,15 @@
 let handler = async(m, { conn, args, command, isOwner }) => {
 	let user = m.mentionedJid[0]
 	let users = global.DATABASE._data.users
+        let z = { contextInfo: { mentionedJid: [user] } }
 if(command == 'block') {
 	conn.blockUser(user, "add")
 	users[user].banned = true
-	m.reply('Berhasil Blockir ' + '@' + user.split("@")[0])
+	m.reply('Berhasil Blockir ' + '@' + user.split("@")[0], z)
 	}else if(command == 'unblock') {
 		conn.blockUser(user, "remove")
 		users[user].banned = false
-        m.reply('Berhasil UnBlockir ' + '@' + user.split("@")[0])
+        m.reply('Berhasil UnBlockir ' + '@' + user.split("@")[0], z)
 }
 conn.on('CB:Blocklist', json => {
     if (global.block.length > 2) return
