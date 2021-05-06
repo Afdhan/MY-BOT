fec444aec1353d40b7cdb26c3997200b97c8056c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-07 04:59:23 +0800

    Update assalam.js

--- plugins/assalam.js
+++ plugins/assalam.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, args }) => {
   let toPTT = convert.toPTT
   let vn = await toPTT(vnn, vnn)*/
   //m.reply('_Waalaikumsalam Kak @${user.split("@")[0]} :)_')
-  conn.sendFile(m.chat, './src/SALAM.opus', Mimetype.ogg)
+  conn.sendFile(m.chat, './src/SALAM.opus', `vn.opus`, null, m, { Audio: false })
 }
 handler.command = new RegExp
 handler.customPrefix = /^(p(unten|ermisi)?|samlekom|hai|halo|hallo)$/i
