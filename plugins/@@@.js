87f98b80bf15fd5f079ffbcba204e1df3c99cc5b
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-06 18:51:41 +0700

    Add files via upload

--- plugins/@@@.js
+++ plugins/@@@.js
@@ -55,9 +55,7 @@ let handler = async (m, { conn, args, text }) => {
        }else if(args.length == 20) {
                 hsl = 'https://wa.me/' + who.split`@`[0] + '&send?text=' + args[0] + '+' + args[1] + '+' + args[2] + '+' + args[3] + '+' + args[4] + '+' + args[5] + '+' + args[6] + '+' + args[7] + '+' + args[8] + '+' + args[9] + '+' + args[10] + '+' + args[11] + '+' + args[12] + '+' + args[13] + '+' + args[14] + '+' + args[15] + '+' + args[16] + '+' + args[17] + '+' + args[18] + '+' + args[19]
 }
-	if (args[0] == m.mentionedJid && args.length == 1) {
-	let hsl
-        if (args.length == 2) {
+       if (args[0] == m.mentionedJid && args.length == 2) {
 	            hsl = 'https://wa.me/' + who.split`@`[0] + '&send?text=' + args[1]
        }else if(args.length == 3) {
 	            hsl = 'https://wa.me/' + who.split`@`[0] + '&send?text=' + args[1] + '+' + args[2]
