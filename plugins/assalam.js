3407bfd18acd7fa423f123e96d02d86f0b48ca59
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-07 02:27:24 +0700

    Update assalam.js

--- plugins/assalam.js
+++ plugins/assalam.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args }) => {
 //let user = m.sender
   let vnn ='src/SALAM.m4a'
   let toPTT = convert.toPTT
-  let vn = await toPTT(vnn, m4a)
+  let vn = await toPTT(vnn.m4a)
   //m.reply('_Waalaikumsalam Kak @${user.split("@")[0]} :)_')
   conn.sendFile(m.chat, vn, 'Assaalamualaikum.opus', null, m, true)
 }
