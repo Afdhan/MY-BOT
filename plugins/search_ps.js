6f2896d5b7f162ad5de3c73a87be8f3fbf14f8a9
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-07 01:30:20 +0700

    Update search_ps.js

--- plugins/search_ps.js
+++ plugins/search_ps.js
@@ -2,14 +2,14 @@ let axios = require("axios");
 let kntl = require("../src/kntl.json")
 let handler = async(m, { conn, text }) => {
       if (!text) return conn.reply(m.chat, '_Mau Cari Apa Tod?_', m)
-      let api = (kntl.onlydev)
+      let api = (kntl.zeks)
       await m.reply(global.wait)
   try {
-  	axios.get(`https://onlydevcity.herokuapp.com/api/googleplay?q=${text}&apikey=${api}`).then ((res) => {
-  	let tytyd = res.data.result.data
+  	axios.get(`https://api.zeks.xyz/api/sgplay?apikey=${api}&q=${text}`).then ((res) => {
+  	let tytyd = res.data.result
         let ppk = `*「  PLAYSTORE SEARCH  」*\n\n`
-        for (let i = 0; i < res.data.result.data.result.length; i++) {
-      	ppk += `─────────────────\n• *Title:* ${res.data.result.data.result[i].title}\n• *AppId:* ${res.data.result.data.result[i].appId}\n• *Developer:* ${res.data.result.data.result[i].developer}\n• *Link:* ${res.data.result.data.result[i].url}\n`
+        for (let i = 0; i < tytyd.length; i++) {
+      	ppk += `─────────────────\n• *Title:* ${tytyd[i].title}\n• *AppId:* ${tytyd[i].appid}\n• *Developer:* ${tytyd[i].developer}\n• *Link:* ${tytyd[i].url}\n`
                 }
         ppk += '\n\n*SGDC-BOT*'
                  conn.reply(m.chat, sfile, m)
