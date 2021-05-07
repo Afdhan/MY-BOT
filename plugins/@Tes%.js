a98cc58276df1ea14dfef948a6b943e30f8787d9
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-07 19:14:17 +0700

    Update @Tes%.js

--- plugins/@Tes%.js
+++ plugins/@Tes%.js
@@ -1,16 +1,22 @@
 let axios = require("axios");
 let kntl = require("../src/kntl.json");
 let handler = async(m, { conn, args, text }) => {
-    //if(args[0].startsWith("08")) return m.reply("_Gunakan Kode Negara
-    if(!text) return m.reply('_Masukan Nomor Telepon Yang Akan Dispam Kode OTP OLX')
+    //if(args[0].startsWith("08")) return m.reply("_Gunakan Kode Negara_")
+    let txt
+    if(args[0] == m.mentionedJid){
+      txt = m.mentionedJid[0]
+    }else{
+      txt = text
+    }
+    if(!txt) return m.reply('_Masukan Nomor Telepon Atau Tag Orang Yang Akan Dispam Kode OTP OLX')
     try {
     await m.reply(global.wait)
     let api = (kntl.xteam)
-	axios.get(`https://api.xteam.xyz/spammer/olx?no=${text}&APIKEY=${api}`)
-	axios.get(`https://api.xteam.xyz/spammer/olx?no=${text}&APIKEY=${api}`)
-	axios.get(`https://api.xteam.xyz/spammer/olx?no=${text}&APIKEY=${api}`)
-	axios.get(`https://api.xteam.xyz/spammer/olx?no=${text}&APIKEY=${api}`)
-	axios.get(`https://api.xteam.xyz/spammer/olx?no=${text}&APIKEY=${api}`)
+	axios.get(`https://api.xteam.xyz/spammer/olx?no=${txt}&APIKEY=${api}`)
+	axios.get(`https://api.xteam.xyz/spammer/olx?no=${txt}&APIKEY=${api}`)
+	axios.get(`https://api.xteam.xyz/spammer/olx?no=${txt}&APIKEY=${api}`)
+	axios.get(`https://api.xteam.xyz/spammer/olx?no=${txt}&APIKEY=${api}`)
+	axios.get(`https://api.xteam.xyz/spammer/olx?no=${txt}&APIKEY=${api}`)
 	.then ((res) => {
 	let hasil = '```SUCCESS SPAM OLX OTP CODE```\n\n*SGDC-BOT*'
         conn.reply(m.chat, hasil, m)
