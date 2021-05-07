ca0ba8e3ff601247180f7a94c1f200ab16c14f41
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-07 19:47:25 +0700

    Update sfiledl.js

--- plugins/sfiledl.js
+++ plugins/sfiledl.js
@@ -1,30 +1,32 @@
 let axios = require("axios");
 let handler = async(m, { conn, text }) => {
-	m.reply('```Kami Butuh Apikey Untuk Memperbaiki Fitur Ini!```')
-/*  try {
+//m.reply('```Kami Butuh Apikey Untuk Memperbaiki Fitur Ini!```')
+  try {
     if (!text) return conn.reply(m.chat, 'Masukkan Link!', m)
     await m.reply(global.wait)
     axios.get(`https://fzn-gaz.herokuapp.com/api/sfiledl?url=${text}`).then ((res) => {
  m.reply(`
 *KLIK LINK FOR DOWNLOAD*
 
-*Title:* ${res.data.title}
-*Size:* ${res.data.size}
-*Link:* ${res.data.result}
+*Title:* ${res.data.result.title}
+*Size:* ${res.data.result.size}
+*Link:* ${res.data.result.downloadURL}
 
 _Download Sendiri, Jangan Manja :v_
 
 *SGDC-BOT*
 `.trim())
-  /*if (res.data.size > '999.99 KB') {
+ /* if (res.data.size > '999.99 KB') {
 	  m.reply('_File Anda Terlalu Besar! Silahkan Download Sendiri Melalui Link Yang Dikirim SGDC-BOT!_')
-	  } else {
-   conn.sendFile(m.chat, res.data.result, 'SGDC-BOT || ' + res.data.title, 'ini', m)
+	  } else {*/
+   let bct = res.data.result.title
+   let ajg = res.data.result.downloadURL
+   conn.sendFile(m.chat, ajg, 'SGDC-BOT || ' + bct, 'ini', m)
 		  //}
-	})
+  })
  } catch (e) {
    m.reply('```Error```')
-  }*/
+  }
 }
 
 handler.command = /^(sfiledl)$/i
