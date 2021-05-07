4923bcae739b2dd9e2415894b5261e6c1ee79276
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-07 19:07:03 +0700

    Update @Tes%.js

--- plugins/@Tes%.js
+++ plugins/@Tes%.js
@@ -1,12 +1,18 @@
 let axios = require("axios");
 let kntl = require("../src/kntl.json");
-let handler = async(m, { conn, text }) => {
+let handler = async(m, { conn, args, text }) => {
+    //if(args[0].startsWith("08")) return m.reply("_Gunakan Kode Negara
     if(!text) return m.reply('_Masukan Nomor Telepon Yang Akan Dispam Kode OTP PizzaHut')
     try {
     await m.reply(global.wait)
     let api = (kntl.xteam)
-	axios.get(`https://api.xteam.xyz/spammer/pizzahut?no=${text}&APIKEY=${api}`).then ((res) => {
-	let hasil = `${res.data.result}\n\n*SGDC-BOT*`
+	axios.get(`https://api.xteam.xyz/spammer/olx?no=${text}&APIKEY=${api}`)
+	axios.get(`https://api.xteam.xyz/spammer/olx?no=${text}&APIKEY=${api}`)
+	axios.get(`https://api.xteam.xyz/spammer/olx?no=${text}&APIKEY=${api}`)
+	axios.get(`https://api.xteam.xyz/spammer/olx?no=${text}&APIKEY=${api}`)
+	axios.get(`https://api.xteam.xyz/spammer/olx?no=${text}&APIKEY=${api}`)
+	.then ((res) => {
+	let hasil = '```SUCCESS```\n\n*SGDC-BOT*'
         conn.reply(m.chat, hasil, m)
      })
    }catch(e){
@@ -15,7 +21,7 @@ let handler = async(m, { conn, text }) => {
    }
 }
 
-handler.command = /^(pizzahut)$/i
+handler.command = /^(olx)$/i
 
 handler.premium = true
 
