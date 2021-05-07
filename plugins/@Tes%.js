6743931feb5fabc2aa1ca9e2cef3dff91a372ecd
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-07 19:32:31 +0700

    Update @Tes%.js

--- plugins/@Tes%.js
+++ plugins/@Tes%.js
@@ -2,14 +2,13 @@ let axios = require("axios");
 let kntl = require("../src/kntl.json");
 let handler = async(m, { conn, args, text }) => {
     //if(args[0].startsWith("08")) return m.reply("_Gunakan Kode Negara_")
-   /* let txt
+    let txt
     if(args[0] == m.mentionedJid){
-      txt = m.mentionedJid[0]
+    let tytyd = m.mentionedJid[0]
+      txt = tytyd.split("@s.whatsapp.net")[0]
     }else{
       txt = args[0]
-    }*/
-    let tytyd = m.mentionedJid[0]
-    let txt = tytyd.split("@s.whatsapp.net")[0]
+    } 
     if(!txt) return m.reply('_Tag Orang Yang Akan Dispam Kode OTP OLX_')
     try {
     await m.reply(global.wait)
