27b4393e95628172b4f537da75053d2f08e5338b
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-07 20:11:59 +0700

    Update sfiledl.js

--- plugins/sfiledl.js
+++ plugins/sfiledl.js
@@ -1,12 +1,15 @@
 let axios = require("axios");
 let kntl = require("../src/kntl.json")
-let handler = async(m, { conn, text }) => {
+let handler = async(m, { conn, args, usedPrefix, text }) => {
   let api = (kntl.xteam)
 //m.reply('```Kami Butuh Apikey Untuk Memperbaiki Fitur Ini!```')
+ if(args.length == 0) return m.reply(`Cara Penggunaan Fitur Sfile Downloader, ${usedPrefix}sfiledl (ext) (url)\nContoh ${usedPrefix}sfiledl hc https://sfile.mobi/taikucing`)
+ if(!args[0]) return m.reply('_Masukkan Extensi File!_')      
+ if(!args[1]) return m.reply('_Masukkan Link Sfile!_')
+   
   try {
-    if (!text) return conn.reply(m.chat, 'Masukkan Link!', m)
     await m.reply(global.wait)
-    axios.get(`https://api.xteam.xyz/dl/sfiledl?url=${text}&APIKEY=${api}`).then ((res) => {
+    axios.get(`https://api.xteam.xyz/dl/sfiledl?url=${args[1]}&APIKEY=${api}`).then ((res) => {
  await m.reply(`
 *KLIK LINK FOR DOWNLOAD*
 
@@ -21,9 +24,14 @@ _Download Sendiri, Jangan Manja :v_
  /* if (res.data.size > '999.99 KB') {
 	  m.reply('_File Anda Terlalu Besar! Silahkan Download Sendiri Melalui Link Yang Dikirim SGDC-BOT!_')
 	  } else {*/
+   let ext
+   if(args[0] == 'hc') ext = 'hc'
+   else if(args[0] == 'ehi' || args[0] == 'hi') ext = 'ehi'
+   else if(args[0] == 'npv2') ext = 'npv2'
+   else throw 'Ekstensi File Tidak Didukung'
    let bct = res.data.result.title
    let ajg = res.data.result.downloadURL
-   conn.sendFile(m.chat, ajg, 'SGDC-BOT || ' + bct, 'ini', m)
+   conn.sendFile(m.chat, ajg, 'SGDC-BOT || ' + bct + ext, 'ini', m)
 		  //}
   })
  } catch (e) {
