a6b7a6d15d870e99f75c4a288c7d276d0ad03462
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-08 20:47:55 +0700

    Update __tesmeme.js

--- plugins/__tesmeme.js
+++ plugins/__tesmeme.js
@@ -12,9 +12,9 @@ let kntl = require("../src/kntl.json")
 let axios = require("axios");
 let handler = async(m, { conn, text, args, bot, command }) => {
     let api = (kntl.lolkey)
-    let [txt1, txt2] = text.split("|")
+    /*let [txt1, txt2] = text.split("|")
     if(!txt1) return m.reply('_Masukkan Teks Satu!_')
-    if(!txt2) return m.reply('_Masukkan Teks Dua!_')
+    if(!txt2) return m.reply('_Masukkan Teks Dua!_')*/
     const type = Object.keys(m.message)[0]
     const content = JSON.stringify(m.message)
     const isMedia = (type === 'imageMessage' || type === 'videoMessage')
@@ -30,8 +30,8 @@ let handler = async(m, { conn, text, args, bot, command }) => {
            /*let [txt1, txt2] = text.split`|`
            if(!txt1) return m.reply('_Masukkan Teks Satu!_')
            if(!txt2) return m.reply('_Masukkan Teks Dua!_')*/
-         //let txt1 = args[0]
-         //let txt2 = 'SGDC-BOT'
+         let txt1 = 'SGDC-TEAM'
+         let txt2 = 'SGDC-BOT'
          //anu.display_url
          let url = 'https://static.wikia.nocookie.net/dogelore/images/9/97/Doge.jpg/revision/latest/top-crop/width/360/height/450?cb=20190205113053'
         let link = 'https://api.lolhuman.xyz/api/memegen?apikey=' + api + '&texttop=' + txt1 + '&textbottom=' + txt2 + '&img=' + anu.display_url;
@@ -42,7 +42,7 @@ let handler = async(m, { conn, text, args, bot, command }) => {
     	 conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
-       } else if (!media) {
+       } else {
         m.reply('_Reply Fotonya!_')
         }
     } catch (e) {
