cdb5367b74ae0277357c4d71cafe5873590833e4
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-08 20:43:07 +0700

    Update __tesmeme.js

--- plugins/__tesmeme.js
+++ plugins/__tesmeme.js
@@ -12,9 +12,9 @@ let kntl = require("../src/kntl.json")
 let axios = require("axios");
 let handler = async(m, { conn, text, args, bot, command }) => {
     let api = (kntl.lolkey)
-    let [txt1, txt2] = text.split("|")
+    /*let [txt1, txt2] = text.split("|")
     if(!txt1) return m.reply('_Masukkan Teks Satu!_')
-    if(!txt2) return m.reply('_Masukkan Teks Dua!_')
+    if(!txt2) return m.reply('_Masukkan Teks Dua!_')*/
     const type = Object.keys(m.message)[0]
     const content = JSON.stringify(m.message)
     const isMedia = (type === 'imageMessage' || type === 'videoMessage')
