10d7694cb5baa878b4903cc6782262ab137e7887
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-08 22:17:46 +0700

    Add files via upload

--- plugins/__protected.js
+++ plugins/__protected.js
@@ -2,7 +2,6 @@ let imgBB = require("imgbb-uploader");
 let { sticker } = require('../lib/sticker');
 let fetch = require('node-fetch');
 let path = require('path');
-let axios = require("axios");
 let util = require('util');
 let fs = require('fs');
 let { MessageType } = require('@adiwajshing/baileys');
@@ -15,14 +14,12 @@ let handler = async (m, { conn, text, args, participants }) => {
 let [s1, s2] = text.split`|`
 if (!s1) return m.reply("Masukkan Username")
 if (!s2) return m.reply("Masukkan Comment")
-
-   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-   let media = await conn.getProfilePicture(who)
-   let res = await axios.get(media)
-   let image = res.data
-   let anu = await imgBB("3ea1465ef91578a90ee81f7d41c59a1f", image)
-   let pict = `${anu.display_url}`
-   let ytc = 'https://some-random-api.ml/canvas/youtube-comment?username=' + s1 + '&comment=' + s2 + '&avatar=' + pict + '&light=true'
+   var imgBB = require('imgbb-uploader')
+    if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
+        ngntd = isQuotedImage ? JSON.parse(JSON.stringify(m).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : m
+        media = await conn.downloadAndSaveMediaMessage(ngntd)
+        anu = await imgBB("3ea1465ef91578a90ee81f7d41c59a1f", media)       
+       let ytc = 'https://some-random-api.ml/canvas/youtube-comment?username=' + s1 + '&comment=' + s2 + '&avatar=' + anu.display_url + '&light=true'
         conn.sendMessage(m.chat, ytc, MessageType.image, { quoted: m, caption: "*SGDC-BOT*" } )
  try {
    let yt = await fetch('https://some-random-api.ml/canvas/youtube-comment?username=' + s1 + '&comment=' + s2 + '&avatar=' + encodeURIComponent(anu.display_url) + '&light=true')
