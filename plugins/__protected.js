4b25b0800903b3bc328583f438ababa6dfed0d15
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-09 01:29:12 +0700

    Add files via upload

--- plugins/__protected.js
+++ plugins/__protected.js
@@ -1,4 +1,5 @@
 let imgBB = require("imgbb-uploader");
+let uploadImage = require('../lib/uploadImage')
 let { sticker } = require('../lib/sticker');
 let fetch = require('node-fetch');
 let path = require('path');
@@ -6,23 +7,16 @@ let util = require('util');
 let fs = require('fs');
 let { MessageType } = require('@adiwajshing/baileys');
 let handler = async (m, { conn, text, args, participants }) => {
-	const type = Object.keys(m.message)[0]
-    const content = JSON.stringify(m.message)
-    const isMedia = (type === 'imageMessage' || type === 'videoMessage')
-    const isQuotedImage = type === 'extendedTextMessage' && content.includes('imageMessage')
-    const isQuotedVideo = type === 'extendedTextMessage' && content.includes('videoMessage')
-let [s1, s2] = text.split`|`
-if (!s1) return m.reply("Masukkan Username")
-if (!s2) return m.reply("Masukkan Comment")
-   var imgBB = require('imgbb-uploader')
-    if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
-        ngntd = isQuotedImage ? JSON.parse(JSON.stringify(m).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : m
-        media = await conn.downloadAndSaveMediaMessage(ngntd)
-        anu = await imgBB("3ea1465ef91578a90ee81f7d41c59a1f", media)       
-       let ytc = 'https://some-random-api.ml/canvas/youtube-comment?username=' + s1 + '&comment=' + s2 + '&avatar=' + anu.display_url + '&light=true'
+	let [s1, s2] = text.split`|`
+    if (!s1) return m.reply("Masukkan Username")
+    if (!s2) return m.reply("Masukkan Comment")
+       let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+       let pp = await conn.getProfilePicture(who)
+       let link = await uploadImage(pp)
+       let ytc = 'https://some-random-api.ml/canvas/youtube-comment?username=' + s1 + '&comment=' + s2 + '&avatar=' + link + '&light=true'
         conn.sendMessage(m.chat, ytc, MessageType.image, { quoted: m, caption: "*SGDC-BOT*" } )
  try {
-   let yt = await fetch('https://some-random-api.ml/canvas/youtube-comment?username=' + s1 + '&comment=' + s2 + '&avatar=' + encodeURIComponent(anu.display_url) + '&light=true')
+   let yt = await fetch('https://some-random-api.ml/canvas/youtube-comment?username=' + s1 + '&comment=' + s2 + '&avatar=' + encodeURIComponent(link) + '&light=true')
    let img = await yt.buffer()
    let stiker = await sticker(img, false, global.packname, global.author)
        conn.sendMessage(m.chat, stiker, MessageType.sticker, {
@@ -32,7 +26,6 @@ if (!s2) return m.reply("Masukkan Comment")
     m.reply('```Error Mengonversi Sticker```')
    // throw e
   }
- }
 }
 
 handler.command = /^(ytc(omm?ent?)?)$/i
