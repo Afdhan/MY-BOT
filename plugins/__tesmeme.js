1d0e2f77525bd97fd306cd72fd7dbaf153c1ce4d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-08 19:47:56 +0700

    Update __tesmeme.js

--- plugins/__tesmeme.js
+++ plugins/__tesmeme.js
@@ -23,23 +23,22 @@ let handler = async(m, { conn, text, args, bot, command }) => {
  try {
     await m.reply(global.wait)
     var imgBB = require('imgbb-uploader')
-  /*  if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
+    if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
         ngntd = isQuotedImage ? JSON.parse(JSON.stringify(m).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : m
         media = await conn.downloadAndSaveMediaMessage(ngntd)
         anu = await imgBB("3ea1465ef91578a90ee81f7d41c59a1f", media)
-           */ let url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaVPHWMTO7jGoZP0QHiqlbODT9Gjxo1HnSug&usqp=CAU'
-            let res = await fetch('https://api.lolhuman.xyz/api/memegen?apikey=' + api + '&texttop=' + txt1 + '&textbottom=' + txt2 + '&img=' + encodeURIComponent(url)) 
+            let url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaVPHWMTO7jGoZP0QHiqlbODT9Gjxo1HnSug&usqp=CAU'
+            let res = await fetch('https://api.lolhuman.xyz/api/memegen?apikey=' + api + '&texttop=' + txt1 + '&textbottom=' + txt2 + '&img=' + encodeURIComponent(anu.display_url)) 
                 //anu.display_url
            let img = await res.buffer()
-    	    //let stiker = await sticker(img, false, global.packname, global.author)
+    	    let stiker = await sticker(img, false, global.packname, global.author)
             // let ress = await imageToBase64(res.data.result)
            // let buf = Buffer.from(ress, 'base64')
            // let str = `*SGDC-BOT*`
-           // conn.sendFile(m.chat, img, 'SGDC-BOT.png', '*SGDC-BOT*', m)
-           conn.sendMessage(m.chat, img, MessageType.image) 
-               /*, {
+           await conn.sendFile(m.chat, img, 'SGDC-BOT.png', '*SGDC-BOT*', m)
+           conn.sendMessage(m.chat, stiker, MessageType.sticker, {          
       quoted: m
-     })*/
+     })
    // }
     } catch (e) {
   	m.reply('```Error```')
