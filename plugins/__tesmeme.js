2ec0a7ea320575fd8e6855bb97cb62d9ddfc29b9
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-08 20:32:33 +0700

    Update __tesmeme.js

--- plugins/__tesmeme.js
+++ plugins/__tesmeme.js
@@ -12,8 +12,8 @@ let kntl = require("../src/kntl.json")
 let axios = require("axios");
 let handler = async(m, { conn, text, args, bot, command }) => {
     let api = (kntl.lolkey)
-    //let [txt1, txt2] = text.split("|")
-    //if(!txt1) return m.reply('_Masukkan Teks Satu!_')
+   // let [txt1, txt2] = text.split("|")
+   // if(!txt1) return m.reply('_Masukkan Teks Satu!_')
     //if(!txt2) return m.reply('_Masukkan Teks Dua!_')
     const type = Object.keys(m.message)[0]
     const content = JSON.stringify(m.message)
@@ -21,20 +21,21 @@ let handler = async(m, { conn, text, args, bot, command }) => {
     const isQuotedImage = type === 'extendedTextMessage' && content.includes('imageMessage')
     const isQuotedVideo = type === 'extendedTextMessage' && content.includes('videoMessage')
    try {
-    /*var imgBB = require('imgbb-uploader')
+    /var imgBB = require('imgbb-uploader')
        if ((isMedia && !m.message.videoMessage || isQuotedImage) && args.length == 0) {
+        await m.reply(global.wait)
         ngntd = isQuotedImage ? JSON.parse(JSON.stringify(m).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : m
         media = await conn.downloadAndSaveMediaMessage(ngntd)
-        anu = await imgBB("3ea1465ef91578a90ee81f7d41c59a1f", media)*/
+        anu = await imgBB("3ea1465ef91578a90ee81f7d41c59a1f", media)
          let txt1 = 'M AFDHAN'
          let txt2 = 'SGDC-BOT'
          //anu.display_url
          let url = 'https://static.wikia.nocookie.net/dogelore/images/9/97/Doge.jpg/revision/latest/top-crop/width/360/height/450?cb=20190205113053'
-        let link = 'https://api.lolhuman.xyz/api/memegen?apikey=' + api + '&texttop=' + txt1 + '&textbottom=' + txt2 + '&img=' + url;
-        let meme = await fetch('https://api.lolhuman.xyz/api/memegen?apikey=' + api + '&texttop=' + txt1 + '&textbottom=' + txt2 + '&img=' + encodeURIComponent(url));
+        let link = 'https://api.lolhuman.xyz/api/memegen?apikey=' + api + '&texttop=' + txt1 + '&textbottom=' + txt2 + '&img=' + anu.display_url;
+        let meme = await fetch('https://api.lolhuman.xyz/api/memegen?apikey=' + api + '&texttop=' + txt1 + '&textbottom=' + txt2 + '&img=' + encodeURIComponent(anu.display_url));
         let img = await meme.buffer()
         let stiker = await sticker(img, false, global.packname, global.author)
-        conn.sendFile(m.chat, link, 'SGDC-BOT.jpg', "*SGDC-BOT*", m)
+        //conn.sendFile(m.chat, link, 'SGDC-BOT.jpg', "*SGDC-BOT*", m)
     	 conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
   })
