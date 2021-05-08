6c41e4374b898cff081fe761948a5498e6494054
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-09 01:36:37 +0700

    tr

--- plugins/__protected.js
+++ plugins/__protected.js
@@ -10,13 +10,14 @@ let handler = async (m, { conn, text, args, participants }) => {
 	let [s1, s2] = text.split`|`
     if (!s1) return m.reply("Masukkan Username")
     if (!s2) return m.reply("Masukkan Comment")
+   await m.reply(global.wait)
        let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw 'No media found'
   let media = await q.download()
        let link = await uploadImage(media)
        let ytc = 'https://some-random-api.ml/canvas/youtube-comment?username=' + s1 + '&comment=' + s2 + '&avatar=' + link + '&light=true'
-        conn.sendMessage(m.chat, ytc, MessageType.image, { quoted: m, caption: "*SGDC-BOT*" } )
+        conn.sendMessage(m.chat, ytc, MessageType.image)  // { quoted: m, caption: "*SGDC-BOT*" } )
  try {
    let yt = await fetch('https://some-random-api.ml/canvas/youtube-comment?username=' + s1 + '&comment=' + s2 + '&avatar=' + encodeURIComponent(link) + '&light=true')
    let img = await yt.buffer()
