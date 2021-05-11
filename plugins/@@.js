2564edf1ecd3ac0b106877902f30adbcfa4a6ecd
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-12 00:22:19 +0700

    Update @@.js

--- plugins/@@.js
+++ plugins/@@.js
@@ -1,11 +1,22 @@
 let handler = async (m, { conn, text, args, command: cmd, usedPrefix: _p }) => {
-	if(m.message > 1) {
+        let prf = `${pickRandom(global.rpf)}`
+	if(cmd.startsWith(_p) || cmd.startsWith(prf)) {
 	let user = m.sender
-       await conn.reply(m.chat, `Maaf @${user.split("@")[0]}, Anda Telah Spam Command, Anda Akan Di Bann Otomatis!`, m, { contextInfo: { mentionedJid: [user]}})
-       let users = global.DATABASE._data.users
-       users[user].banned = true
+        conn.reply(m.chat, `Maaf @${user.split("@")[0]}, Command *${m.text}* Tidak Ditemukan`, m, { contextInfo: { mentionedJid: [user]}})
+       /*let users = global.DATABASE._data.users
+       users[user].banned = true*/
        }
    }
 handler.command = new RegExp
 
 module.exports = handler
+
+global.rpf = [
+"♤","◇","~","●","@","•","♡",",","♧",
+"○","¥","+","+","×","%","$","☆","/","^",
+"#","?","€","7","*","Q","0","Z",".","1","!","-",
+]
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
