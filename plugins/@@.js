6b42ea3daa4f651c41093f36c61182af160fca1c
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-12 00:12:29 +0700

    Update @@.js

--- plugins/@@.js
+++ plugins/@@.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text, args, command: cmd, usedPrefix: _p }) => {
-	if(m.message.startsWith(_p).length >= 1) {
+	if(m.message.length > 1) {
 	let user = m.sender
        await conn.reply(m.chat, `Maaf @${user.split("@")[0]}, Anda Telah Spam Command, Anda Akan Di Bann Otomatis!`, m, { contextInfo: { mentionedJid: [user]}})
        let users = global.DATABASE._data.users
