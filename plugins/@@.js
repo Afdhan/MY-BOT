f1cf4ae46d90e61295c1edcf947e954583330b29
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-12 00:40:31 +0700

    Update @@.js

--- plugins/@@.js
+++ plugins/@@.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, text, args, command: cmd, usedPrefix: _p }) => {
         let prf = `${pickRandom(global.rpf)}`
         let ch = m.message
-	if(cmd.startsWith(_p + cmd) || cmd.startsWith(prf + cmd) === undefined) {
+        //if(cmd.startsWith(_p + cmd) || cmd.startsWith(prf + cmd) == undefined) {
 	let user = m.sender
         conn.reply(m.chat, `Maaf @${user.split("@")[0]}, Command *${m.text}* Tidak Ditemukan`, m, { contextInfo: { mentionedJid: [user]}})
        /*let users = global.DATABASE._data.users
        users[user].banned = true*/
-       }
+       //}
    }
 handler.command = new RegExp
 
