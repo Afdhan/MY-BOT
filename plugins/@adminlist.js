d81ca9f5f9649974d36d915afce31a9a96454537
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-13 14:51:03 +0700

    Update @adminlist.js

--- plugins/@adminlist.js
+++ plugins/@adminlist.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn, args, participants }) => {
     let nama = await conn.getName(m.key.remoteJid)
     let groupAdmins = m.isGroup ? getGroupAdmins(participants) : ''
     let txt = `List Admin Group *${nama}*\nTotal : ${groupAdmins.length}\n\n`
-    let no = '> 0.'
+    let no = 0
     for (let admon of groupAdmins) {
-    no += '> 1.'
+    no += 1
     txt += `[${no.toString()}] @${admon.split('@')[0]}\n`
       }
     txt += '\n\n*SGDC-BOT*'
