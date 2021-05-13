0f23da6d1f015c93b43bf0d0874e5828f4a76ec7
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-13 14:55:57 +0700

    Update @adminlist.js

--- plugins/@adminlist.js
+++ plugins/@adminlist.js
@@ -1,23 +1,23 @@
 let handler = async (m, { conn, args, participants }) => {
     let nama = await conn.getName(m.key.remoteJid)
-    let groupAdmins = m.isGroup ? getGroupAdmins(participants) : ''
-    let txt = `List Admin Group *${nama}*\nTotal : ${groupAdmins.length}\n\n`
+    let mimin = m.isGroup ? NgeriAtmin(participants) : ''
+    let txt = `List Admin Group *${nama}*\n*Total:* ${mimin.length}\n\n`
     let no = 0
-    for (let admon of groupAdmins) {
+    for (let tod of mimin) {
     no += 1
-    txt += `[${no.toString()}] @${admon.split('@')[0]}\n`
+    txt += `> [${no.toString()}] @${tod.split('@')[0]}\n`
       }
     txt += '\n\n*SGDC-BOT*'
-conn.reply(m.chat, txt, m, { contextInfo: { mentionedJid: groupAdmins }})
+conn.reply(m.chat, txt, m, { contextInfo: { mentionedJid: mimin }})
 }
 handler.command = /^(adminlist|listadmin)$/i
-
+handler.group = true
 module.exports = handler
 
-const getGroupAdmins = (participants) => {
-	admins = []
-	for (let i of participants) {
-		i.isAdmin ? admins.push(i.jid) : ''
+const NgeriAtmin = (participants) => {
+        atminn = []
+	for (let b of participants) {
+             b.isAdmin ? atminn.push(b.jid) : ''
 	}
-	return admins
+	return atminn
 }
