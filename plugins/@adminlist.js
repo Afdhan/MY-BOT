417549afe4ca4b4e679f0eecbd414fb54ef9d618
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-13 14:49:01 +0700

    Update @adminlist.js

--- plugins/@adminlist.js
+++ plugins/@adminlist.js
@@ -1,8 +1,9 @@
-let handler = async (m, { conn, args, isAdmin, }) => {
+let handler = async (m, { conn, args, participants }) => {
     let nama = await conn.getName(m.key.remoteJid)
-    let txt = `List Admin Group *${nama}*\nTotal : ${isAdmin.length}\n\n`
+    let groupAdmins = m.isGroup ? getGroupAdmins(participants) : ''
+    let txt = `List Admin Group *${nama}*\nTotal : ${groupAdmins.length}\n\n`
     let no = '> 0.'
-    for (let admon of isAdmin) {
+    for (let admon of groupAdmins) {
     no += '> 1.'
     txt += `[${no.toString()}] @${admon.split('@')[0]}\n`
       }
@@ -12,3 +13,11 @@ conn.reply(m.chat, txt, m, { contextInfo: { mentionedJid: isAdmin }})
 handler.command = /^(adminlist|listadmin)$/i
 
 module.exports = handler
+
+const getGroupAdmins = (participants) => {
+	admins = []
+	for (let i of participants) {
+		i.isAdmin ? admins.push(i.jid) : ''
+	}
+	return admins
+}
