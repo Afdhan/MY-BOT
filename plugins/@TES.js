4fedc4de086591986977808926a01bc95f15d02d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-15 06:48:02 +0700

    Update @TES.js

--- plugins/@TES.js
+++ plugins/@TES.js
@@ -10,7 +10,7 @@ let handler = async(m, { conn, args, participants }) => {
   })
     let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
     console.log(participants)
-    conn.reply(m.chat, `${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Level ${level}*`).join`\n, m)
+    conn.reply(m.chat, `${sortedLevel.slice(0, len).map(({ jid, level }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *Level ${level}*`).join`\n`}, m)
  }
 
 handler.command = /^(data)$/i
