94f8a4461b4326656b402fea4ce0addc53d7e532
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-15 06:18:18 +0700

    Add files via upload

---
+++ plugins/@TES.js
@@ -0,0 +1,14 @@
+let handler = async(m, { conn, args }) => {
+	let data = global.DATABASE._data.users
+	let out = `*DATA USERS*`
+	for (let i = 0; i < data.length; i++) {
+       out += `${data[i]}\n`
+    }
+    conn.reply(m.chat, out, m)
+ }
+
+handler.command = /^(data)$/i
+
+module.exports = handler
+
+//  MUHAMMAD AFDHAN
