d8d9454f941d60e017691883aad61cbb6495ac0d
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-15 19:16:53 +0700

    Update @gabut.js

--- plugins/@gabut.js
+++ plugins/@gabut.js
@@ -3,7 +3,7 @@ let MessageType = require("@adiwajshing/baileys");
 let { image } = MessageType
 let CreateLink = require('../lib/CreateLink')
 let handler = async (m, { conn, args, text }) => {
-let user = Object.values(global.DATABASE._data.users).filter(user => user.number).length
+let user = Object.values(global.DATABASE._data.users).number.map(u => u.jid)
 //let tes = await CreateLink(user, text)
 m.reply(user)
 }
