7adef7a5ace206250dea6928e553220857c35535
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-16 01:24:05 +0700

    Update @@tes.js

--- plugins/@@tes.js
+++ plugins/@@tes.js
@@ -2,9 +2,10 @@ let handler = async (m, { conn, text, args }) => {
   let data = await conn.groupMetadata(m.chat)
   let hsl = JSON.stringify(data, null, 1)
   let yy = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
-  //let dua = await conn.loadAllMessages(yy)
+  let dua = yy.length
+      //await conn.loadAllMessages(yy)
   for (let i = 0; i < yy.length; i++) {
-      let dua += await conn.loadAllMessages(yy[i])
+      dua = await conn.loadAllMessages(yy[i])
     }
   m.reply(dua)
 }
