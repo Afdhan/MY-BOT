4b110a0554f80fd4534f18566047ddc51ee98420
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-15 19:38:46 +0700

    Update @gabut.js

--- plugins/@gabut.js
+++ plugins/@gabut.js
@@ -3,13 +3,35 @@ let MessageType = require("@adiwajshing/baileys");
 let { image } = MessageType
 let CreateLink = require('../lib/CreateLink')
 let handler = async (m, { conn, args, text }) => {
-let data = Object.values(global.DATABASE._data.users[m.sender]).filter(user => user.number)
-let out = `*DATA USERS*`
-	for (let i = 0; i < data.length; i++) {
-       out += `${data[i]}\n`
-    }
+let data = Object.entries(global.DATABASE._data.users).map(([key, value]) => {
+    return {...value, jid: key}
+  })
+let dia = data.map(toNumber('number')).sort(sort('number'))
+let makhluk = dia.map(enumGetKey)
+let out = dia.slice(0).map(({ jid }) => `*>* @${jid.split`@`[0]}`).join`\n`
+
 //let tes = await CreateLink(user, text)
-m.reply(`${out}`)
+m.reply(out, {
+    contextInfo: {
+      mentionedJid: [...makhluk.slice(0)]
+    }
+  })
 }
 handler.command = /^(tes)$/
 module.exports = handler
+
+function sort(property, ascending = true) {
+  if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
+  else return (...args) => args[ascending & 1] - args[!ascending & 1]
+}
+
+function toNumber(property, _default = 0) {
+  if (property) return (a, i, b) => {
+    return {...b[i], [property]: a[property] === undefined ? _default : a[property]}
+  }
+  else return a => a === undefined ? _default : a
+}
+
+function enumGetKey(a) {
+  return a.jid
+}
