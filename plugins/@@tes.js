01dd8d8a0356c3edebf0c1e98edccfcbb1f51043
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-16 01:20:37 +0700

    Update @@tes.js

--- plugins/@@tes.js
+++ plugins/@@tes.js
@@ -1,7 +1,12 @@
 let handler = async (m, { conn, text, args }) => {
   let data = await conn.groupMetadata(m.chat)
   let hsl = JSON.stringify(data, null, 1)
-  m.reply(hsl)
+  let yy = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
+  let dua = await conn.loadAllMessages(yy)
+  for (let i = 0; i < yy.length; i++) {
+      dua += await conn.loadAllMessages(yy[i])
+    }
+  m.reply(dua)
 }
 handler.command = /^tes$/
 module.exports = handler
