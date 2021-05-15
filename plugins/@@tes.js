91f47dbf59dd8c7ff0c7f588d9417244d7986f35
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-16 01:25:19 +0700

    Update @@tes.js

--- plugins/@@tes.js
+++ plugins/@@tes.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text, args }) => {
   let dua = yy.length
       //await conn.loadAllMessages(yy)
   for (let i = 0; i < yy.length; i++) {
-      dua = await conn.loadAllMessages(yy[i])
+      dua = await conn.loadAllMessages([i])
     }
   m.reply(dua)
 }
