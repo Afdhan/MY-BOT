73f12f86925fab48982f9e3b30b2091370cee2bc
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-16 06:35:09 +0700

    Update @@tes.js

--- plugins/@@tes.js
+++ plugins/@@tes.js
@@ -2,11 +2,11 @@ let handler = async (m, { conn, text, args }) => {
   let data = await conn.groupMetadata(m.chat)
   let hsl = JSON.stringify(data, null, 1)
   let yy = Object.values(global.DATABASE._data.users).filter(user => user.registered == false)
-  let dua = 'TES\n\n'
+  /*let dua = 'TES\n\n'
       //await conn.loadAllMessages(yy)
   for (let i = 0; i < yy.length; i++) {
       dua = `@${yy[i].split("@")[0]}`
-    }
+    }*/
   let tuga = await conn.groupMetadataMinimal(m.chat)
   m.reply(tuga)
 }
