31f9ad5d2ab9350812ec20456a3e4c9aa574703e
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-15 21:46:17 +0700

    Update @gabut.js

--- plugins/@gabut.js
+++ plugins/@gabut.js
@@ -1,21 +1,38 @@
 let axios = require("axios");
 let MessageType = require("@adiwajshing/baileys");
 let { image } = MessageType
+let kntl = require("../src/kntl.json")
+let fetch = require('node-fetch')
 let CreateLink = require('../lib/CreateLink')
-let handler = async (m, { conn, args, text }) => {
-let data = Object.entries(global.DATABASE._data.users).map(([key, value]) => {
-    return {...value, jid: key}
-  })
-let dia = data.map(toNumber('number')).sort(sort('number'))
-let makhluk = data.map(enumGetKey)
-let out = dia.slice(0).map(({ jid }) => `*>* @${jid.split`@`[0]}`).join`\n`
+let handler = async (m, { conn, args, text, command }) => {
+    let api = (kntl.lolkey)
+    if (!text) return m.reply("_Masukkan Link XNXX_")
+  try {
+let res = await fetch(`https://api.lolhuman.xyz/api/xnxx?apikey=${api}&url=${text}`)
+let json = await res.json()
+let data = json.result
+let url = data.link
 
-//let tes = await CreateLink(user, text)
-m.reply(out, {
-    contextInfo: {
-      mentionedJid: [...makhluk.slice(0)]
-    }
-  })
+let txt = `
+*Title:* ${data.title}
+*Duration:* ${data.duration}
+*views:* ${data.view}
+*Rating:* ${data.rating}
+*Like:* ${data.like}
+*Dislike:* ${data.dislike}
+*Comment:* ${data.comment}
+*tag:* ${data.tag.join(",")}
+*Description:* ${data.description}
+
+*LINK*
+${url}
+`.trim()
+for (let x of url) {
+      txt += `${x.type} - ${x.link}\n\n`
+ }
+      let ress = await fetch(data.thumbnail)
+      let thum = await ress.buffer()
+      conn.sendFie(m.chat, thum, txt, m)
 }
 handler.command = /^(tes)$/
 module.exports = handler
