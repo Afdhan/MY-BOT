409f8bf9c2562e6d4d06b3a66022310a34525299
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-15 22:32:36 +0700

    Update @gabut.js

--- plugins/@gabut.js
+++ plugins/@gabut.js
@@ -6,7 +6,11 @@ let fetch = require('node-fetch')
 let CreateLink = require('../lib/CreateLink')
 let handler = async (m, { conn, args, text, command }) => {
     let api = (kntl.lolkey)
+    let chat = global.DATABASE.data.chats[m.chat]
+    if (chat.nsfw) { 
+    if(command == 'xnxx' || command == 'xnxxdl') {
     if (!text) return m.reply("_Masukkan Link XNXX_")
+        await m.reply(global.wait)
   try {
 let res = await fetch(`https://api.lolhuman.xyz/api/xnxx?apikey=${api}&url=${text}`)
 let json = await res.json()
@@ -24,7 +28,7 @@ let txt = `
 *Like:* ${data.like}
 *Dislike:* ${data.dislike}
 *Comment:* ${data.comment}
-*tag:* ${data.tag.join("\n")}
+*Tag:* ${data.tag.join(", ")}
 *Description:* ${data.description}
 
 *DOWNLOAD*
@@ -32,33 +36,43 @@ let txt = `
 
 for (let i = 0; i < data.link.length; i++) {
     txt += `\nType: ${data.link[i].type}\n`
-    txt += `Link: ${data.link[i].link}\n\n`
+    txt += `Link: ${data.link[i].link}\n`
    }
     txt += '\n*SGDC-BOT*'
       //let ress = await fetch(data.thumbnail)
      // let thum = await ress.buffer()
-      await m.reply(txt)
-      conn.sendFile(m.chat, data.thumbnail, txt, m)
+      //await m.reply(txt)
+      conn.sendFile(m.chat, data.thumbnail, "STOPCOLY.jpg", txt, m)
       }catch(e){
           m.reply ("ERROR")
           console.log (e)
        }
-}
+    } else if (command == 'xnxxsearch' || command == 'searchxnxx') {
+      if (!text) return m.reply("_Masukkan Kata Kunci_")
+      await m.reply(global.wait)
+     try {
+      let res = await fetch(`https://api.lolhuman.xyz/api/xnxxsearch?apikey=${api}&query=${text}`)
+      let json = res.json()
+      let ress = json.result
+      let hsl = `*[ XNXX SEARCH ]*\n\n`
+      for (let i = 0; i < ress.length; i++) {
+           hsl += `*Title:* ${ress[i].title}\n`
+           hsl += `*Views:* ${ress[i].views}\n`
+           hsl += `*Duration:* ${ress[i].duration}\n`
+           hsl += `*Uploader:* ${ress[i].uploader}\n`
+           hsl += `*Download:*\n${ress[i].link}\n`
+         }
+           hsl += '\n*SGDC-BOT*'
+        conn.reply(m.chat, hsl, m)
+    }catch(e){
+        m.reply("ERROR")
+        console.log(e)
+     }
+    } else {
+       m.reply('```Perlu Mengaktifkan Mode NSFW```')
+    }
+ }
 handler.command = /^(tes)$/
 module.exports = handler
 
-function sort(property, ascending = true) {
-  if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
-  else return (...args) => args[ascending & 1] - args[!ascending & 1]
-}
-
-function toNumber(property, _default = 0) {
-  if (property) return (a, i, b) => {
-    return {...b[i], [property]: a[property] === undefined ? _default : a[property]}
-  }
-  else return a => a === undefined ? _default : a
-}
-
-function enumGetKey(a) {
-  return a.jid
-}
+//M AFDHAN
