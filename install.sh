250a3a4ec4b0ddf6f2fef78d1ffce17f60d2d225
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-23 18:10:36 +0700

    Update Versi 1.6.5

--- install.sh
+++ install.sh
@@ -1,17 +1,12 @@
+#/usr/bin/bash
+
 pkg install nodejs -y
 pkg install ffmpeg -y
-pkg install imagemagick -y
+pkg install libwebp -y
 pkg install wget -y
-
-apt-get install nodejs -y
-apt-get install libwebp -y
-apt-get install ffmpeg -y
-apt-get install wget -y
 npm install
 npm audit fix
-
 clear
-
 echo ""
 echo ""
 echo "=+=+=+=+=+=+=+=+÷+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
