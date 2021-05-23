3a9dac26638b35f2c51aacfb5182808bcbd75cdb
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-23 22:36:22 +0700

    Update sticker_search.js

--- plugins/sticker_search.js
+++ plugins/sticker_search.js
@@ -12,8 +12,8 @@ try {
     	let stic = res.json()
         let tytyd = stic.result.stickerlist
 	   for (let x of tytyd) {
-        let img = x[Math.floor(Math.random() * x.length)];
-        let stiker = await sticker(null, img, global.packname, global.author)
+        //let img = x[Math.floor(Math.random() * x.length)];
+        let stiker = await sticker(false, x, global.packname, global.author)
        conn.sendMessage(m.chat, stiker, MessageType.sticker, {
     quoted: m
         })
