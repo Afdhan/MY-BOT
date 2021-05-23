2d91f1697bfb85c0b0ddd4c350a8d69f68afc703
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-23 10:44:02 +0700

    Add files via upload

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -1,23 +1,24 @@
 let handler = m => m
 handler.before = m => {
   let user = global.DATABASE.data.users[m.sender]
-  let kon = m.sender
   if (user.afk > -1) {
     m.reply(`
-_@${kon.split("@")[0]} berhenti AFK!_
-`.trim(), { contextInfo: { mentionedJid: [kon] } })
+Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
+Selama ${clockString(new Date - user.afk)}
+`.trim())
     user.afk = -1
     user.afkReason = ''
   }
-  let jids = [...new Set(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])]
+  let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
   for (let jid of jids) {
     let user = global.DATABASE.data.users[jid]
     if (!user) continue
     let afkTime = user.afk
     if (!afkTime || afkTime < 0) continue
-    let reason = user.afkReason || 'AFK'
+    let reason = user.afkReason || ''
     m.reply(`
-_Jangan tag dia!, dia lagi ${reason} 
+Jangan tag dia!
+Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
 Selama ${clockString(new Date - afkTime)}
 `.trim())
   }
