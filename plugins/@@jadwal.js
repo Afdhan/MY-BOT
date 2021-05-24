aeb78bb8a5119fe3e8665785095420e15e13b2e1
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-24 19:11:44 +0700

    Update

---
+++ plugins/@@jadwal.js
@@ -0,0 +1,34 @@
+const { MessageType } = require("@adiwajshing/baoleys")
+let handler = (m, { conn }) => {
+	var date = new Date();
+
+var bulan = date.getMonth();
+var tanggal = date.getDate();
+var hari = date.getDay();
+var jam = date.getHours();
+var menit = date.getMinutes();
+var detik = date.getSeconds();
+
+let chats = conn.chats.all().filter(v => !v.read_only && v.message && !v.archive).map(v => v.jid)
+//let content = await conn.cMod(m.chat, m, /bc|broadcast/i.test(text) ? text : text + '\n' + readMore + '「 All Chat Broadcast 」')
+let str = `
+_Selamat Adzan Maghrib Untuk Wilayah D.K.I Jakarta Dan Sekitarnya..._
+
+_*SGDC-BOT* Auto Reminder_
+`.trim()
+
+if (jam === 17 && menit === 49) {
+  for (let id of chats) conn.copyNForward(id, str, true) 
+//conn.sendMessage(chats, str, MessageType.text)
+     }
+}
+
+/*
+      "Shubuh": "04:35",
+      "Dzuhur": "11:52",
+      "Ashr": "15:14",
+      "Maghrib": "17:46",
+      "Isya": "19:00"
+*/
+
+module.exports = handler
---
+++ plugins/@imagefilters.js
@@ -0,0 +1,40 @@
+const uploadImage = require('../lib/uploadImage')
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  await m.reply(global.wait)
+try {
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada foto'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  let img = await q.download()
+  let url = await uploadImage(img)
+  let stick = `https://some-random-api.ml/canvas/${command}?avatar=${url}`
+  await conn.sendFile(m.chat, stick, "SGDC-BOT.jpg", "*SGDC-BOT*",  m)
+  let stiker = await sticker(null, stick, global.packname, global.author)
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+} catch (e) {
+  m.reply('```ERROR```')
+  console.log(e)
+  }
+}
+
+
+handler.command = [
+'greyscale',
+'invert',
+'brightness',
+'threshold',
+'sepia',
+'red',
+'green',
+'blue',
+'blurple',
+'pixelate',
+'blur',
+]
+
+module.exports = handler
