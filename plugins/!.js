0db0006c06d84644121625f203dd9884ac6381ba
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-24 21:21:06 +0700

    Create !.js

---
+++ plugins/!.js
@@ -0,0 +1,16 @@
+const { uploadimg: imag } = require("../lib/uploadimg")
+let handler = (m, { conn, text, args }) => {
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (/image|video/.test(mime)) {
+      let img = await q.download()
+      if (!img) throw '_Tipe Tidak Diketahui!_'
+      let hasil = await imag(img)
+      m.reply('link: ' + hasil)
+     }catch(e){
+        console.log (e)
+    }
+}
+handler.command = /^tes$/i
+module.exports = handler
