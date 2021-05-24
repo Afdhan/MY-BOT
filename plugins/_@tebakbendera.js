07f0e9e090f7d60d918d45cd21fed88b9fd6c5d3
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-24 15:04:38 +0700

    Add files via upload

---
+++ plugins/_@tebakbendera.js
@@ -0,0 +1,41 @@
+const { MessageType } = require('@adiwajshing/baileys')
+const fetch = require('node-fetch')
+const { sticker } = require('../lib/sticker')
+let timeout = 120000
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
+        throw false
+    }
+    let res = await fetch(global.API('xteam', '/game/tebakbendera', {}, 'APIKEY'))
+    if (res.status !== 200) throw await res.text()
+    let json = await res.json()
+    if (!json.status) throw json
+    let stiker = await sticker(null, global.API('xteam', '/sticker/emojitopngwhatsapp', { emo: geprek }, 'APIKEY'), `Bendera Apa?`, `Hayoloh..`)
+    let caption = `
+\`\`\`TEBAK BENDERA\`\`\`
+Bendera Negara Manakah Ini? : ${json.bendera}
+
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik *${usedPrefix}wun* untuk bantuan
+
+_Balas Pesan Ini Untuk Menjawab!_
+`.trim()
+    conn.tebakbendera[id] = [
+        await m.reply(caption),
+        conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+     quoted: m
+     }),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `_Waktu habis! Jawabannya adalah *${json.jawaban}*_`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
+    await conn.sendFile(m.chat, json.preview, 'coba-lagi.mp3', '', m)
+}
+
+handler.command = /^tebakbendera$/i
+module.exports = handler
---
+++ plugins/_@tebakbendera_jwb.js
@@ -0,0 +1,21 @@
+let handler = m => m
+handler.before = async function (m) {
+    let id = m.chat
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/tebakbendera/i.test(m.quoted.text)) return !0
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    if (!(id in conn.tebakbendera)) return m.reply('Lu Telat Ngab :)')
+    if (m.quoted.id == conn.tebakbendera[id][0].id) {
+        let json = JSON.parse(JSON.stringify(conn.tebakbendera[id][1]))
+        if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
+            global.DATABASE._data.users[m.sender].exp += conn.tebakbendera[id][2]
+            m.reply("```Benar!```")
+            clearTimeout(conn.tebakbendera[id][3])
+            delete conn.tebakbendera[id]
+        } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply("```Dikit Lagi!```")
+        else m.reply("```Salah!```")
+    }
+    return !0
+}
+handler.exp = 0
+
+module.exports = handler
