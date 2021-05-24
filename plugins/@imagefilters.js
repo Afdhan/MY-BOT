d4d960220163f2a09f0ca26d568c511466fca9db
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-24 21:33:43 +0700

    Update @imagefilters.js

--- plugins/@imagefilters.js
+++ plugins/@imagefilters.js
@@ -9,8 +9,8 @@ try {
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw 'Tidak ada foto'
   if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
-  let img = await conn.downloadM(q)
-  let url = await uploadimg(img)
+  let img = await q.download()
+  let url = await uploadImage(img)
   let stick = `https://some-random-api.ml/canvas/${command}?avatar=${url}`
   await conn.sendFile(m.chat, stick, "SGDC-BOT.jpg", "*SGDC-BOT*",  m)
   let stiker = await sticker(null, stick, global.packname, global.author)
