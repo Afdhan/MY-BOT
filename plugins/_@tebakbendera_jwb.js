f956f9e7ddc511852f7eefede9c9f27d728ba714
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-25 10:25:03 +0700

    Update _@tebakbendera_jwb.js

--- plugins/_@tebakbendera_jwb.js
+++ plugins/_@tebakbendera_jwb.js
@@ -1,3 +1,4 @@
+const { sticker } = require('../lib/sticker')
 let handler = m => m
 handler.before = async function (m) {
     let id = m.chat
@@ -7,8 +8,12 @@ handler.before = async function (m) {
     if (m.quoted.id == conn.tebakbendera[id][0].id) {
         let json = JSON.parse(JSON.stringify(conn.tebakbendera[id][1]))
         if (m.text.toLowerCase() == json.jawaban.toLowerCase()) {
-            global.DATABASE._data.users[m.sender].exp += conn.tebakbendera[id][2]
-            m.reply("```Benar!```")
+            //global.DATABASE._data.users[m.sender].exp += conn.tebakbendera[id][2]
+            let stiker = await sticker(null, global.API('xteam', '/sticker/emojitopngwhatsapp', { emo: json.bendera }, 'APIKEY'), 'Jawabannya', `${json.jawaban}`)
+            await m.reply("```Benar!```")
+            conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+     quoted: m
+     })
             clearTimeout(conn.tebakbendera[id][3])
             delete conn.tebakbendera[id]
         } else if (m.text.toLowerCase().endsWith(json.jawaban.split` `[1])) m.reply("```Dikit Lagi!```")
