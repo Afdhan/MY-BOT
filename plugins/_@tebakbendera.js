1ccec90708e4ac04cd2c39095c7ba195d0a33a60
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-05-25 09:34:57 +0700

    Update _@tebakbendera.js

--- plugins/_@tebakbendera.js
+++ plugins/_@tebakbendera.js
@@ -5,10 +5,7 @@ let timeout = 120000
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
-    if (id in conn.tebakbendera) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
-        throw false
-    }
+    if (id in conn.tebakbendera) return conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakbendera[id][0])
     let res = await fetch(global.API('xteam', '/game/tebakbendera', {}, 'APIKEY'))
     if (res.status !== 200) throw await res.text()
     let json = await res.json()
